dffffa5ab5256e488019b68c93b4f265e46cc35f
Tyler <sirtytyz@gmail.com>
2020-07-07 14:58:32 -0500

    updated site meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Family, Folklore, and other Cultural Notes',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'A site dedicated to the family lineage of the Coxs and their kin. Additional information about Arkansas and other notable locations where these families made their mark.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
